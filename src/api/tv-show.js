0f2271f28193d84e808369e8d2d731e44c6f4dd1
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-07-04 14:33:49 -0400

    Feat: fixing features
    It was fixing some features in the app, also was deleted all descriptive comments

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,55 +1,36 @@
 import axios from "axios";
 import { BASE_URL, API_KEY_PARAM } from "../config";
-//import { FAKE_POPULARS } from "./fake_data";
 
 export class TVShowAPI {
-  // AXIOS
-  static async fetchPopulars() {
-    try {
-      const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-      console.log(response.data.results);
-      return response.data.results;
-    } catch (e) {
-      console.log(e);
-    }
-    //return FAKE_POPULARS;
-  }
+	static async fetchPopulars() {
+		try {
+			const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+			console.log(response.data.results);
+			return response.data.results;
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  // FETCH
-  /*static async fetchPopulars() {
-    try {
-      const response = await fetch(`${BASE_URL}tv/popular${API_KEY_PARAM}`, {
-        method: "GET",
-      });
+	static async fetchByTitle(title) {
+		try {
+			const response = await axios.get(
+				`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`
+			);
+			return response.data.results;
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-      const results = await response.json();
-      console.log(results.results);
-      return results.results;
-    } catch (e) {
-      console.log(e);
-    }
-    //return FAKE_POPULARS;
-  }*/
-
-  static async fetchByTitle(title) {
-    try {
-      const response = await axios.get(
-        `${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`
-      );
-      return response.data.results;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  static async fetchRecommendations(tvShowId) {
-    try {
-      const response = await axios.get(
-        `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`
-      );
-      return response.data.results;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	static async fetchRecommendations(tvShowId) {
+		try {
+			const response = await axios.get(
+				`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`
+			);
+			return response.data.results;
+		} catch (e) {
+			console.log(e);
+		}
+	}
 }
--- src/components/FiveStarRating/FiveStarRating.jsx
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -1,31 +1,23 @@
 import { StarFill, Star as StarEmpty } from "react-bootstrap-icons";
 
 const FiveStarRating = ({ rating }) => {
-  // rating = 4.45
+	const starList = [];
 
-  // Declare star icon array
-  const starList = [];
+	const starFillCount = Math.floor(rating);
 
-  // Store number of filled stars
-  const starFillCount = Math.floor(rating); // 4
+	const hasHalfStar = rating - parseInt(rating) >= 0.5;
 
-  // Store if yes o no there is a half star
-  const hasHalfStar = rating - parseInt(rating) >= 0.5; // 4.45 - 4 = 0.45 >= 0.5 = false
+	const emptyStarCount = 5 - starFillCount - (hasHalfStar ? 1 : 0); // 5 - 4 = 1 - 0 = 1
 
-  // Store the number of empty stars
-  const emptyStarCount = 5 - starFillCount - (hasHalfStar ? 1 : 0); // 5 - 4 = 1 - 0 = 1
+	for (let i = 1; i <= starFillCount; i++) {
+		starList.push(<StarFill key={"star-fill" + i} />);
+	}
 
-  // Push the filled stars icons
-  for (let i = 1; i <= starFillCount; i++) {
-    starList.push(<StarFill key={"star-fill" + i} />);
-  }
+	for (let i = 1; i <= emptyStarCount; i++) {
+		starList.push(<StarEmpty key={"star-empty" + i} />);
+	}
 
-  // Push the empty star icons
-  for (let i = 1; i <= emptyStarCount; i++) {
-    starList.push(<StarEmpty key={"star-empty" + i} />);
-  }
-
-  return <div>{starList}</div>;
+	return <div>{starList}</div>;
 };
 
 export default FiveStarRating;
